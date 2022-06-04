62ecc186b11632b10c70d74af014f85c2033db94
jobb-rodriguez <jobb.rodriguez22@gmail.com>
2022-06-04 10:50:53 +0800

    Address disconnect event

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ io.on('connection', (socket) => {
     socket.on('ready', () => {
         console.log('Player ready', socket.id);
         readyPlayerCount++;
-        if (readyPlayerCount === 2) {
+        if (readyPlayerCount % 2 === 0) {
             io.emit('startGame', socket.id);
         }
     })
@@ -30,5 +30,9 @@ io.on('connection', (socket) => {
 
     socket.on('ballMove', (ballData) => {
         socket.broadcast.emit('ballMove', ballData);
+    });
+
+    socket.on('disconnect', (reason) => {
+        console.log(`Client ${socket.id} disconnected: ${reason}`);
     });
 });
