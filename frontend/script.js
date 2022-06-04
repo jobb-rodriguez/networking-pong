8e4b8838cdf68b00475d2c0ce9cff99c6f4f2b94
jobb-rodriguez <jobb.rodriguez22@gmail.com>
2022-06-04 10:23:40 +0800

    Sync ball and score

--- frontend/script.js
+++ frontend/script.js
@@ -90,6 +90,12 @@ function ballReset() {
   ballX = width / 2;
   ballY = height / 2;
   speedY = 3;
+
+  socket.emit('ballMove', {
+    ballX,
+    ballY,
+    score
+  });
 }
 
 // Adjust Ball Movement
@@ -100,6 +106,12 @@ function ballMove() {
   if (playerMoved) {
     ballX += speedX;
   }
+
+  socket.emit('ballMove', {
+    ballX,
+    ballY,
+    score
+  });
 }
 
 // Determine What Ball Bounces Off, Score Points, Reset Ball
@@ -155,9 +167,11 @@ function ballBoundaries() {
 
 // Called Every Frame
 function animate() {
-  ballMove();
+  if (isReferee) {
+    ballMove();
+    ballBoundaries();
+  }
   renderCanvas();
-  ballBoundaries();
   window.requestAnimationFrame(animate);
 }
 
@@ -204,4 +218,8 @@ socket.on('startGame', (refereeId) => {
 socket.on('paddleMove', (paddleData) => {
   opponentPaddleIndex = 1 - paddleIndex
   paddleX[opponentPaddleIndex] = paddleData.xPosition;
-})
+});
+
+socket.on('ballMove', (ballData) => {
+  ({ ballX, ballY, score } = ballData);
+});
