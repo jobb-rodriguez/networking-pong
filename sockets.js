cf239c3147864e32a9b24dc8c4c923df36a48664
jobb-rodriguez <jobb.rodriguez22@gmail.com>
2022-06-04 11:20:11 +0800

    Added rooms

--- sockets.js
+++ sockets.js
@@ -1,28 +1,35 @@
+let readyPlayerCount = 0;
+
 function listen(io) {
     // const pongNamespace = io.of('/pong');
     // replace ios with pongNamespace
-    let readyPlayerCount = 0;
     io.on('connection', (socket) => {
+        let room = 'room' + Math.floor(readyPlayerCount / 2);
         console.log('a user connected: ', socket.id);
 
         socket.on('ready', () => {
-            console.log('Player ready', socket.id);
+            socket.join(room);
+
+            console.log('Player ready', socket.id, room);
             readyPlayerCount++;
             if (readyPlayerCount % 2 === 0) {
-                io.emit('startGame', socket.id);
+                // original had no "in(room)"
+                io.in(room).emit('startGame', socket.id);
             }
         })
 
         socket.on('paddleMove', (paddleData) => {
-            socket.broadcast.emit('paddleMove', paddleData);
+            // original used broadcast instead of to(room)
+            socket.to(room).emit('paddleMove', paddleData);
         });
 
         socket.on('ballMove', (ballData) => {
-            socket.broadcast.emit('ballMove', ballData);
+            socket.to(room).emit('ballMove', ballData);
         });
 
         socket.on('disconnect', (reason) => {
             console.log(`Client ${socket.id} disconnected: ${reason}`);
+            socket.leave(room);
         });
     });
 }
